7baa23a73ea0225563a87434af30931a56693da6
IWAN ARIANA <38771364+iwanariana@users.noreply.github.com>
2023-08-14 17:53:10 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,10 @@
-// 1. Save the files to the user's device
-// The "install" event is called when the ServiceWorker starts up.
-// All ServiceWorker code must be inside events.
 self.addEventListener('install', function(e) {
     console.log('install');
   
-    // waitUntil tells the browser that the install event is not finished until we have
-    // cached all of our files
     e.waitUntil(
-      // Here we call our cache "myonsenuipwa", but you can name it anything unique
       caches.open('myonsenuipwa').then(cache => {
-        // If the request for any of these resources fails, _none_ of the resources will be
-        // added to the cache.
         return cache.addAll([
-          '/gis-pudamts',
+          '/gis-pudamts/',
           '/gis-pudamts/index.html',
           'https://unpkg.com/onsenui/css/onsenui.min.css',
           'https://unpkg.com/onsenui/css/onsen-css-components.min.css',
@@ -29,18 +21,19 @@ self.addEventListener('install', function(e) {
           '/gis-pudamts/icon/Map.svg',
           '/gis-pudamts/icon/Mag.svg',
           '/gis-pudamts/icon/Res.svg',         
-         
         ]);
       })
     );
-  });
-  
-  // 2. Intercept requests and return the cached version 6 instead
-  self.addEventListener('fetch', function(e) {
+});
+
+self.addEventListener('fetch', function(e) {
     e.respondWith(
-      // check if this file exists in the cache
       caches.match(e.request)
-        // Return the cached file, or else try to get it from the server
-        .then(response => response || fetch(e.request))
+        .then(response => {
+          if (response) {
+            return response; // Return the cached response if available
+          }
+          return fetch(e.request); // Otherwise, fetch from network
+        })
     );
-  });
+});
